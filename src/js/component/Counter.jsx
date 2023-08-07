36864d9428d247ec2195c7d3809bb6800eaecf6e
Sandra Rodríguez <133523307+srdgz@users.noreply.github.com>
2023-08-07 19:18:49 +0000

    primera subida

---
+++ src/js/component/Counter.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+//include images into your bundle
+
+
+//create your first component
+const Counter = (num) => {
+	return (
+		<div className="mainBox">
+			<div className="box"><i class="fa-regular fa-clock"></i></div>
+			<div className="box">{num.num6}</div>
+			<div className="box">{num.num5}</div>
+			<div className="box">{num.num4}</div>
+			<div className="box">{num.num3}</div>
+			<div className="box">{num.num2}</div>
+			<div className="box">{num.num1}</div>
+		</div>
+	);
+};
+export default Counter;
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,23 @@
 //import react into the bundle
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from 'react-dom';
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import Counter from "./component/Counter.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let counter = 0;
+setInterval(() => {
+    ReactDOM.render(<Counter 
+        num1 = {Math.floor((counter/1)%10)}
+        num2 = {Math.floor((counter/10)%10)}
+        num3 = {Math.floor((counter/100)%10)}
+        num4 = {Math.floor((counter/1000)%10)}
+        num5 = {Math.floor((counter/10000)%10)}
+        num6 = {Math.floor((counter/100000)%10)}
+        />, document.querySelector("#app"));
+        counter++
+}, 1000);
